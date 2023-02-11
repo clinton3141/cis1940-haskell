2e2050c9fbd0e29869ba8a37a51535c2664a19ba
Clinton Montague <iblamefish@gmail.com>
2023-02-11 14:41:29 +0000

    Week 1 exercise 1

--- Week 1/HW01.hs
+++ Week 1/HW01.hs
@@ -5,11 +5,11 @@ module HW01 where
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = undefined
+lastDigit = flip mod 10
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit = undefined
+dropLastDigit = flip div 10
 
 -- Exercise 2 -----------------------------------------
 
